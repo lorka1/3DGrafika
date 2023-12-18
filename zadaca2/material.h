a539f41e0f8c3588dc59e2d735178b879daac89d
Lorena <134442286+lorka1@users.noreply.github.com>
2023-12-19 00:02:37 +0100

    Add files via upload

---
+++ zadaca2/material.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "geometry.h"
+
+class Material
+{
+  public:
+    Vec3f diffuse_colour;
+    float diffuse_coef = 1;
+    
+    float specular_coef = 1;
+    float phong_exp = 1000;
+    float reflectivity_coef = 0.1;
+    
+    Material() : diffuse_colour(Vec3f(0, 0, 0)) { }
+    Material(const Vec3f &colour) : diffuse_colour(colour) { }
+};
